11eb7fc97db902ecbf48b21fd44a97a017f792e1
Madz No <madzno12@gmail.com>
2022-10-16 18:39:37 -0400

    adding a post route

--- todo.rb
+++ todo.rb
@@ -2,15 +2,29 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+configure do
+  enable :sessions
+  set :session_secret, "ab9f53e4f022b9d54e8039267fc7704a32ba20279636acb6dfc49ab4840ae3bc"
+end
+
+before do
+  session[:lists] ||= []
+end
+
 get "/" do
   redirect "/lists"
 end
 
 get "/lists" do
-  @lists = [
-    {name: "Lunch Groceries", todos: []},
-    {name: "Dinner Groceries", todos:[]}
-  ]
-
+  @lists = session[:lists]
   erb :lists, layout: :layout
+end
+
+get "/lists/new" do
+  erb :new_list, layout: :layout
+end
+
+post "/lists" do
+  session[:lists] << {name: params[:list_name], todos: []}
+  redirect "/lists"
 end
